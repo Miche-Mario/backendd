df107b6a41ff63148f49985d89f9e770734fa971
Miche Mario <micheganhou@gmail.com>
2022-10-13 09:33:34 +0000

    kjnkjnj

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -3,8 +3,18 @@ import {Login, logOut, Me} from '../controllers/Auth.js'
 
 const router = express.Router();
 
-router.get('/me', Me);
-router.post('/login', Login);
+router.get('/me', Me).options(function(req,res,next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); return res.send(200);
+
+});
+router.post('/login', Login).options(function(req,res,next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET, POST');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); return res.send(200);
+
+});
 router.delete('/logout', logOut);
 
 
