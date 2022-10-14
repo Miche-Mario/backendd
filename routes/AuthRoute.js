6fddfedecc8e34d05a4764ecbd85636dc7792d71
Miche Mario <micheganhou@gmail.com>
2022-10-14 15:10:57 +0000

    kjdksd

--- routes/AuthRoute.js
+++ routes/AuthRoute.js
@@ -3,8 +3,8 @@ import {Login, logOut, Me} from '../controllers/Auth.js'
 
 const router = express.Router();
 
-router.get('/me', Me)
-router.post('/login', Login)
+router.get('/me', Me);
+router.post('/login', Login);
 router.delete('/logout', logOut);
 
 
