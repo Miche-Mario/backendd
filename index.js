bd0d7cd8df1535616246789d4d935e644ec6f5b0
Miche Mario <micheganhou@gmail.com>
2022-10-15 12:59:38 +0000

    ooo

--- index.js
+++ index.js
@@ -49,6 +49,9 @@ app.use(session({
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', 'true');
+
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+   
     next();
   });
 
