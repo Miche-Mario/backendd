d4c69af6dda675af1d0ef6c99461f93b44f91f85
Miche Mario <micheganhou@gmail.com>
2022-10-15 07:49:16 +0000

    ooo

--- index.js
+++ index.js
@@ -27,9 +27,9 @@ const store = new sessionStore({
     db: db
 });
 
-// (async()=>{
-//     await db.sync();
-// })();
+(async()=>{
+    await db.sync();
+})();
 
 app.use(session({
     secret: process.env.SESS_SECRET,
