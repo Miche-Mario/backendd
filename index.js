348ce4eb14f7491a769188e4f3481eb0938e0caf
Miche Mario <micheganhou@gmail.com>
2022-10-18 13:08:05 +0000

    hgh

--- index.js
+++ index.js
@@ -36,24 +36,25 @@ app.use(session({
     saveUninitialized: true,
     store: store,
     cookie: {
-        secure: 'auto'
+        secure: true,
+        sameSite: 'none'
     }
 }));
 
-/* app.use(cors({
+app.use(cors({
     credentials: true,
     origin: 'http://localhost:3000'
-})); */
+}));
 
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
     res.header('Access-Control-Allow-Credentials', 'true');
 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
    
     next();
-  });
+  }); */
 
 app.use(UsersRoute);
 app.use(TauxRoute);
