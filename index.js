11375fa28a202dd20afc9f03e2a69b73a74260b4
Miche Mario <micheganhou@gmail.com>
2022-10-18 08:12:37 +0000

    gittt

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ app.use(session({
 
 
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', 'http://localhost:3000');
+    res.header('Access-Control-Allow-Origin', 'https://ouando.com');
     res.header('Access-Control-Allow-Credentials', 'true');
 
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
