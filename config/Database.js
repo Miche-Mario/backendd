f2294748b5c974c60d98a6dc8d112fd3c6564e38
Miche Mario <micheganhou@gmail.com>
2022-12-30 11:32:20 +0100

    changedb

--- config/Database.js
+++ config/Database.js
@@ -1,8 +1,8 @@
 import { Sequelize } from "sequelize";
 
 
-const db = new Sequelize('db_a8e19e_loan','a8e19e_loan','Lostoflove90&', {
-    host: 'mysql8001.site4now.net',
+const db = new Sequelize('db_a92562_loan','a92562_loan','Loanloan90', {
+    host: 'MYSQL8002.site4now.net',
     dialect: "mysql"
 });
 
